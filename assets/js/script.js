670f8597959d26593e8b4632a5f193d5ac8465b6
vijaylaxmipb <vijaylaxmipb@gmail.com>
2024-03-14 15:50:19 +0000

    Add code to increment score and incorrect answers talley

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,8 +41,10 @@ function checkAnswer(){
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -62,13 +64,25 @@ function calculateCorrectAnswer(){
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
-
 }
+
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 
 function incrementScore(){
-
+     let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer(){
+
+    
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 function displayAdditionQuestion(){
