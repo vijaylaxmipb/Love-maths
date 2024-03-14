a2a964c1536b2419707c01417d09fd1e31400c9a
vijaylaxmipb <vijaylaxmipb@gmail.com>
2024-03-14 09:24:08 +0000

    Add code to calculate random numbers to game question

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded",function(){
+    let buttons = document.getElementsByTagName("button");
+    for(let button of buttons){
+        button.addEventListener("click",function(){
+            if (this.getAttribute("data-type") === "submit") {
+            alert("You clicked Submit!");
+        } else {
+            let gameType = this.getAttribute("data-type");
+            alert(`You clicked ${gameType}`);
+        }
+
+        });
+    }
+});
+
+function runGame(){
+    //create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random{} * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    runGame();
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
+
+
+    
