7d6bd3fc4dc392cf57a46e8ddcb016d8a0d67eb8
vijaylaxmipb <vijaylaxmipb@gmail.com>
2024-03-14 10:34:08 +0000

    Add get element and inner text to get correct answer to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,17 @@ function checkAnswer(){
 
 function calculateCorrectAnswer(){
 
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+
 }
 
 function incrementScore(){
